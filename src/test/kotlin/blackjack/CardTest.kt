e0d4c52a5618c3f378ee7e2fcde31b148fa0bb0b
palmseung <palmseung@gmail.com>
2021-03-14 16:52:23 +0900

    test(Card): 카드는 번호와 문양으로 구성된다.

---
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -0,0 +1,30 @@
+package blackjack
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class CardTest {
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    fun `카드는 번호(CardNumber)와 문양(CardSymbol)로 생성된다`(cardNumber: CardNumber, cardSymbol: CardSymbol) {
+        // when, then
+        assertDoesNotThrow { Card(cardNumber, cardSymbol) }
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideParameters(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(CardNumber.ACE, CardSymbol.CLOVER),
+                Arguments.of(CardNumber.FIVE, CardSymbol.DIAMOND),
+                Arguments.of(CardNumber.KING, CardSymbol.HEART),
+                Arguments.of(CardNumber.QUEEN, CardSymbol.SPADE)
+            )
+        }
+    }
+}
