8ebd07490e49c073f521e528e31c8246a046d965
palmseung <palmseung@gmail.com>
2021-03-14 16:40:56 +0900

    feat(CardNumber): 블랙잭 카드 번호 종류 enum 생성

---
+++ src/main/kotlin/blackjack/CardNumber.kt
@@ -0,0 +1,16 @@
+package blackjack
+
+enum class CardNumber(vararg point: Int) {
+    ACE(1, 11),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
+}
