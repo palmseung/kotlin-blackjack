d84a6ac795aaca9a77b3e7a1b34add128f4adfd0
palmseung <palmseung@gmail.com>
2021-03-16 00:39:11 +0900

    feat(Deck): 하나의 Deck은 52장의 카드를 갖는다.

--- src/main/kotlin/blackjack/Deck.kt
+++ src/main/kotlin/blackjack/Deck.kt
@@ -1,5 +1,25 @@
 package blackjack
 
 class Deck {
+    private var _cards = mutableListOf<Card>()
 
+    var cards: List<Card> = CARDS_CLOVER
+        get() = _cards
+
+    init {
+        _cards = CARDS
+    }
+
+    companion object {
+        private var CARDS_SPADE = CardNumber.values().map { Card(it, CardSymbol.SPADE) }.toMutableList()
+        private var CARDS_HEART = CardNumber.values().map { Card(it, CardSymbol.HEART) }.toMutableList()
+        private var CARDS_CLOVER = CardNumber.values().map { Card(it, CardSymbol.CLOVER) }.toMutableList()
+        private var CARDS_DIAMOND = CardNumber.values().map { Card(it, CardSymbol.DIAMOND) }.toMutableList()
+        private var CARDS = CARDS_CLOVER.add(CARDS_DIAMOND).add(CARDS_HEART).add(CARDS_SPADE).toMutableList()
+
+        private fun MutableList<Card>.add(cards: List<Card>): MutableList<Card> {
+            this.addAll(cards)
+            return this
+        }
+    }
 }
