152bf03c60b1629b76e27e3d145e67060b873d66
palmseung <palmseung@gmail.com>
2021-03-16 01:55:42 +0900

    test(BlackJackGame): player가 가진 카드가 없다면, 2장이 player에게 지급된다.

--- src/main/kotlin/blackjack/BlackJackGame.kt
+++ src/main/kotlin/blackjack/BlackJackGame.kt
@@ -4,4 +4,8 @@ class BlackJackGame(val players: Players, val deck: Deck) {
     fun turn(): Player {
         return players.turn()
     }
+
+    fun play(thisTurnPlayer: Player): Player {
+        TODO("Not yet implemented")
+    }
 }
--- src/test/kotlin/blackjack/BlackJackGameTest.kt
+++ src/test/kotlin/blackjack/BlackJackGameTest.kt
@@ -38,4 +38,19 @@ internal class BlackJackGameTest {
         assertThat(player2.name).isEqualTo("Frank")
         assertThat(player3.name).isEqualTo("Jae")
     }
+
+    @Test
+    fun `Player가 가진 카드가 한 장도 없다면 play할 때 두 장의 카드가 주어진다`() {
+        // given
+        val deck = Deck()
+        val players = Players(listOf("Kent", "Frank", "Jae"))
+        val blackJackGame = BlackJackGame(players, deck)
+        val thisTurnPlayer = blackJackGame.turn()
+
+        // when
+        val player = blackJackGame.play(thisTurnPlayer)
+
+        // then
+        assertThat(player.cards.size).isEualto(2)
+    }
 }
